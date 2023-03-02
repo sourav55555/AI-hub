b88833c50ac35593f1becbec19a7543196c62285
sourav55555 <souravsinghassr555@gmail.com>
2023-03-02 21:21:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const ai = async()=>{
+    try{
+        const req = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+        const data = await req.json();
+        if(showall){
+            data.data.map(data => c)
+        }
+    }
+}
