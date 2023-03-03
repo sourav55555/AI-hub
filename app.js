48262dec9495e85d565f496cf646365e7e017256
sourav55555 <souravsinghassr555@gmail.com>
2023-03-03 10:38:28 +0600

    sort by date

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const spinner = document.getElementById("spinner");
 
-const ai = async(showall)=>{
+let datesSort = [];
+
+// fetch data
+const ai = async(showall,dateSort)=>{
     try{
         const req = await fetch('https://openapi.programming-hero.com/api/ai/tools');
         const data = await req.json();
         const datas =  data.data.tools;
+        datesSort = datas;
+        console.log(dateSort);
+        
         if(showall){
-           datas.map(data => displayAllAi(data));
-           spinner.classList.add("hidden");
+            if(dateSort){
+
+                console.log("entered else", dateSort);
+                dateSort.map(data => displayAllAi(data));
+                spinner.classList.add("hidden");
+
+                
+            }
+            else{
+                datas.map(data => displayAllAi(data));
+                spinner.classList.add("hidden");
+            }
+           
         }
         else{
-            datas.slice(0,6).map(data => displayAllAi(data));
-            seeMore.classList.remove("hidden");
-            spinner.classList.add("hidden");
+            if(dateSort){
+                dateSort.slice(0,6).map(data => displayAllAi(data));
+                seeMore.classList.remove("hidden");
+                spinner.classList.add("hidden");
+         
+            }
+            else{
+                datas.slice(0,6).map(data => displayAllAi(data));
+                seeMore.classList.remove("hidden");
+                spinner.classList.add("hidden");
+            }
+            
         }
     }
     catch(error){
@@ -20,8 +46,43 @@ const ai = async(showall)=>{
     }
 }
 
+
+// date sorting
+const sortCustom = (a , b)=>{
+    const date1 = new Date(a.published_in);
+    const date2 = new Date(b.published_in);
+
+    if(date1 > date2){
+        return 1;
+    }
+    else if(date1 < date2){
+        return -1;
+    }
+    else{
+        return 0;
+    }
+}
+
+let connectShowAll = [];
+const sortByDate = document.getElementById("sort-date");
+sortByDate.addEventListener("click",function(){
+    cards.innerText='';
+    const sorted = datesSort.sort(sortCustom);
+    if(connectToSort){
+        ai(true,sorted);
+    }
+    else{
+        ai("",sorted);
+    }
+   
+    connectShowAll = sorted;
+})
+
+
+
 const cards = document.getElementById("cards");
 
+// display all cards
 const displayAllAi = data =>{
 
     const div = document.createElement("div");
@@ -32,9 +93,9 @@ const displayAllAi = data =>{
     <div class="border-b-2 pb-4">
         <p class="mb-3 text-xl font-semibold">Features</p>
         <ol class="list-decimal ml-5">
-            <li class="text-gray-600 text-sm">${features[0]}</li>
-            <li class="text-gray-600 text-sm">${features[1]}</li>
-            <li class="text-gray-600 text-sm">${features[2]}</li>
+        <li class="text-gray-600 ${features[0] ? "" : "hidden"} text-sm">${features[0] ? features[0] : ""}</li>
+            <li class="text-gray-600 ${features[1] ? "" : "hidden"} text-sm">${features[1] ? features[1] : ""}</li>
+            <li class="text-gray-600 ${features[2] ? "" : "hidden"} text-sm">${features[2] ? features[2] : ""}</li>
         </ol>
         
     </div>
@@ -65,11 +126,21 @@ document.querySelector(".modal-close").addEventListener("click",function(){
     modal.classList.add("hidden");
 })
 
+
+// show all
+let connectToSort = "";
 const seeMore = document.getElementById("seeMore");
 seeMore.addEventListener("click", function(){
-    ai(true);
+    if(connectShowAll.length == 0){
+        ai(true,false);
+    }
+    else{
+        ai(true,connectShowAll)
+    }
     seeMore.classList.add("hidden");
     spinner.classList.remove("hidden");
+    cards.innerText='';
+    connectToSort = true;
 })
 
-ai();
+ai(false,false);
